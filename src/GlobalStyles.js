a4f1ec5bca6c629ff3bc117fbd775ca7f25bdc88
Nog <edrogonzales@hotmail.com>
2020-12-29 16:22:20 -0800

    Navbar branch initialization

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from "styled-components";
+import DancingScript from "./assets/fonts/DancingScript.ttf";
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'DancingScript';
+    src: url(${DancingScript});
+    font-weight: normal;
+    font-style: normal;
+  }
+
+
 * {
   box-sizing: border-box;
   margin: 0;
