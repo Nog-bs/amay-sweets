806a108524894302bf6707746587572c25a6f8fa
livedriedmango <jimmyjamesyngson@gmail.com>
2020-12-30 19:47:25 -0800

    Gallery cards started.

--- src/components/Gallery/Gallery.elements.js
+++ src/components/Gallery/Gallery.elements.js
@@ -3,14 +3,19 @@ import styled from "styled-components";
 export const GalleryBox = styled.div`
   display: flex;
   justify-content: center;
-  background: #ffe4ea;
+
   width: 100;
   height: 90vh;
 `;
 
 export const GalleryCard = styled.div`
+  background-image: url(${({ data }) => data.img1});
+  background-size: cover;
   height: 350px;
   width: 300px;
-  margin: 50px;
-  background: #fff;
+  background-repeat: no-repeat;
+  margin: 6px;
+  border: 2px solid pink;
+  border-radius: 10px;
+  box-shadow: 0px 6px 10px grey;
 `;
---
+++ src/data/GalleryData.js
@@ -0,0 +1,10 @@
+import BasqueBurntCheesecake1 from "../assets/Images/Basque_Burnt_Cheesecake.PNG";
+import BasqueBurntCheesecake2 from "../assets/Images/Basque_Burnt_Cheesecake_2.jpg";
+
+export const data = [
+  {
+    name: "Basque Burnt Cheesecake",
+    img1: BasqueBurntCheesecake1,
+    img2: BasqueBurntCheesecake2,
+  },
+];
