e859cc8aa5bcfaff97195ad091c92879d64c1cbb
livedriedmango <jimmyjamesyngson@gmail.com>
2021-01-04 21:03:04 -0800

    Contact page fully implemented. Mobile friendly and desktop ready.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,33 @@
 import React from "react";
+import {
+  Image,
+  ContactCard,
+  ContactImage,
+  Box1,
+  Box2,
+  ContactHead,
+  ContactHr,
+  ContactDesc,
+} from "../styles/Contact.elements";
 
 const Contact = () => {
   return (
     <>
-      <h3>Contact</h3>
+      <Image>
+        <ContactCard>
+          <Box1>
+            <ContactImage />
+          </Box1>
+          <Box2>
+            <ContactHead>What are you waiting for?</ContactHead>
+            <ContactHr />
+            <ContactDesc>
+              Send us a message on Instagram, or email us directly with your
+              orders!
+            </ContactDesc>
+          </Box2>
+        </ContactCard>
+      </Image>
     </>
   );
 };
